a7443e31f8548d5aa07c8f963b4ce89d5496df40
Tim Hua <tim@jolata.com>
2015-04-16 15:13:55 -0700

    updated app.js to healthStatusMonitor.js

---
+++ healthStatusMonitor.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var process = require('process');
+var utils = require('./lib/util.js');
+var app = express();
+var jconf = '';
+
+if(process.argv[2]){
+  jconf = process.argv[2] + '/etc/jolata.conf';
+} else {
+  jconf = '/home/jolata/etc/jolata.conf';
+}
+
+// local flag
+if(process.argv[3] == '-l'){
+  var local = true;
+}
+// required to handle larger json responses
+// app.use(bodyParser.json({limit: '5mb'}));
+
+app.post('/api/brokerStatus', utils.brokerStatus);
+app.post('/api/coordinatorStatus', utils.coordinatorStatus);
+app.post('/api/historicalStatus', utils.historicalStatus);
+app.post('/api/rtStatus', utils.rtStatus);
+
+console.log("Using Jolata conf file:",jconf);
+require('./lib/middleware.js')(jconf, local);
+
+if(!local){
+  console.log("listening on port 4000");
+  app.listen(4000);
+}
+
+// //////////////////////////////////////////////////////////////////////
+// // testing only, used to display data received from druid nodes //////
+// app.use(express.static(__dirname + '/public'));
+// // app.use(bodyParser.json());
+// app.get('/', function(req,res){
+//   var query = url.parse(req.url, true).query;
+//   console.log(query);
+//   res.send('index.html');
+//   res.sendStatus(200);
+// });
+
+// app.get('/api/getData', function(req,res){
+//   res.send(data);
+// });
+//////////////////////////////////////////////////////////////////////
