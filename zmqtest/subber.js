770af8746a3f265d95c1faa5188df67eeaf38ab3
Tim Hua <tim@jolata.com>
2015-04-20 10:29:38 -0700

    new diskspace draft

--- zmqtest/subber.js
+++ zmqtest/subber.js
@@ -2,11 +2,13 @@ var express = require('express');
 var StringDecoder = require('string_decoder').StringDecoder;
 var app = express();
 var decoder = new StringDecoder('utf8');
+// var sys = require('sys'),
+//     exec = require('child_process').exec;
 
 var zmq = require('zmq'),
     sock = zmq.socket('sub');
 
-// sock.connect('tcp://192.168.20.220:4101');     //peavy
+sock.connect('tcp://192.168.20.220:4101');     //peavy
 sock.connect('tcp://127.0.0.1:10050');            //localhost
 // sock.connect('tcp://192.168.20.250:10250');       //sogard
 sock.subscribe('');
@@ -17,6 +19,11 @@ sock.on('message', function(topic, chunk) {
   var dataChunk = decoder.write(chunk);
   console.log('containing message:', dataChunk);
 });
+
+
+
+
+
 
 app.listen(4001);
 console.log('listening on port 4001');
